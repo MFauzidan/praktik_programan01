651a202e8f8292aa3cd6f429cc8e4e0124d62b53
M fauzidan <mfzidannn@gmail.com>
2021-09-19 02:41:28 +1000

    tugas identitas mahasiswa

---
+++ pertemuan 3/Identitas_mahasiswa.py
@@ -0,0 +1,26 @@
+# membuat variabel
+
+
+nama = "M fauzidan"
+NIM  = 21537144013
+Angkatan = 2021
+Prodi = "Teknologi Informasi"
+Jurusan = "JPTEI"
+Fakultas = "Teknik"
+IPK_harapan = 4.0
+
+
+# mencatak data diri 
+
+
+print("Nama        = " + nama)
+print("NIM         = " + str(NIM) )
+print("Angkatan    = " + str(Angkatan))
+print("Prodi       = " + Prodi)
+print("Jurusan     = " + Jurusan)
+print("Fakultas    = " + Fakultas)
+print("IPK harapan = " + str(IPK_harapan))
+
+
+
+
