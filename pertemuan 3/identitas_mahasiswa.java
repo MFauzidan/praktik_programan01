6662af87ddddffe807489c52feffd93d4820dc32
M fauzidan <mfzidannn@gmail.com>
2021-09-19 02:40:28 +1000

    tugas identitas saya

---
+++ pertemuan 3/identitas_mahasiswa.java
@@ -0,0 +1,25 @@
+public class identitas_mahasiswa{
+    public static void main(String[] args){
+       
+        // variabel data diri
+
+        String nama = "M fauzidan";
+        String NIM  = "21537144013";
+        int  Angkatan = 2021;
+        String Prodi = "Teknologi Informasi";
+        String Jurusan = "JPTEI";
+        String Fakultas = "Teknik";
+        double IPK_harapan = 4.0;
+        
+        // mencetak data diri
+
+        System.out.println("Nama        = " + nama);
+        System.out.println("NIM         = " + NIM);
+        System.out.println("Angkatan    = " + Angkatan);
+        System.out.println("Prodi       = " + Prodi);
+        System.out.println("Jurusan     = " + Jurusan);
+        System.out.println("Fakultas    = " + Fakultas);
+        System.out.println("IPK harapan = " + IPK_harapan);
+
+    }
+}
