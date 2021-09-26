6a2f96372a2b11588dcdeb2a03fac5a0ab1ab6c9
M fauzidan <mfzidannn@gmail.com>
2021-09-27 00:29:08 +1000

    tugas

---
+++ pertemuan 4/cobainput.py
@@ -0,0 +1,7 @@
+print("masukan nama anda")
+namaDepan = input()
+print("masukan umur anda")
+umur = input()
+
+print("nama saya adalah " + namaDepan)
+print("umur saya adalah " + umur)
---
+++ pertemuan 4/tugas.py
@@ -0,0 +1,19 @@
+
+nilaiA = input("masukan nilaiA : ")
+
+nilaiB = input("masukan nilaiB : ")
+
+penjumlahan = (int(nilaiA) + int(nilaiB))
+pengurangan = (int(nilaiA) - int(nilaiB))
+perkalian = (int(nilaiA) * int(nilaiB))
+pembagian = (int(nilaiA) / int(nilaiB))
+modulus = (int(nilaiA) % int(nilaiB))
+
+
+print("hasil penjumlahan dari " + str(nilaiA) + " + " + str(nilaiB) + " = " + str(penjumlahan) )
+print("hasil pengurangan dari " + str(nilaiA) + " - " + str(nilaiB) + " = " + str(pengurangan) )
+print("hasil perkalian dari " + str(nilaiA) + " * " + str(nilaiB) + " = " + str(perkalian) )
+print("hasil pembagian dari " + str(nilaiA) + " / " + str(nilaiB) + " = " + str(pembagian) )
+print("hasil modulus dari " + str(nilaiA) + " % " + str(nilaiB) + " = " + str(modulus) )
+
+
